58cf2205932b62f99c31d3afe18fc67ebb734559
CLOCKWORK-TEMPTATION <clockworktemptation@gmail.com>
2025-11-13 11:38:35 +0200

    Delete webapp directory

--- webapp/frontend/src/components/agent-reports-exporter.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export interface AgentReportsExporterProps {
-  reports: Record<string, any>;
-  originalText?: string;
-  onExport?: (format: string) => void;
-}
-
-export const AgentReportsExporter: React.FC<AgentReportsExporterProps>;
